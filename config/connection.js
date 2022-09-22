704d452744548e4113fdd77932efed5255c753e6
Kevin Umayam <kevin.m.umayam@gmail.com>
2022-09-22 13:43:35 -0700

    changes to home routes

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3310
+      port: 3310,
     }
   );
 }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,6 +6,7 @@ router.get('/', withAuth, async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const postData = await Techpost.findAll({
+      attributes: { exclude: ['password'] },
       include: [
         {
           model: User,
@@ -23,6 +24,39 @@ router.get('/', withAuth, async (req, res) => {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    //do i need req.params.id here?
+    const postData = await Techpost.findByPk({
+      where: {
+        id: req.session.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['User'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const post = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('single-post', {
+      post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
